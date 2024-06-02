87dfd93b99b8f7c3c3ba9ca4aecaad44959d1911
Kaushal Kumar Yadav <kaushalyad321@gmail.com>
2024-06-02 12:19:12 +0545

    deployed project

--- src/components/Professional.jsx
+++ src/components/Professional.jsx
@@ -11,28 +11,41 @@ const Professional = () => {
       <div className="mt-4">
         <div className="flex">
           <div className="flex justify-center items-center bg-gray-800 py-2 mobile:px-3 tablet:px-5 rounded-lg">
-            <img src="./logos/notebook.svg" className=" mobile: w-4  tablet:w-5" />
-            <p className="ml-2 mobile:text-sm tablet:xl">Certification Programme</p>
+            <img
+              src="./logos/notebook.svg"
+              className=" mobile: w-4  tablet:w-5"
+            />
+            <p className="ml-2 mobile:text-sm tablet:xl">
+              Certification Programme
+            </p>
           </div>
           <div className="flex justify-between items-center mobile:ml-2  tablet:ml-10">
             <img src="./logos/yellowStar.svg" className="w-4" />
-            <p className="text-yellow-400 ml-2  mobile:text-md tablet:text-xl">Rated 4.8/5</p>
-          </div>    
+            <p className="text-yellow-400 ml-2  mobile:text-md tablet:text-xl">
+              Rated 4.8/5
+            </p>
+          </div>
         </div>
         <div></div>
       </div>
       <div className="mt-4 flex flex-col gap-1">
         <div className="flex">
           <img src="./logos/rightTick.svg" className="w-6" />
-          <p className="ml-2 mobile:text-sm tablet:text-xl">India's No.1 Online Makeup Course</p>
+          <p className="ml-2 mobile:text-sm tablet:text-xl">
+            India's No.1 Online Makeup Course
+          </p>
         </div>
         <div className="flex">
           <img src="./logos/rightTick.svg" className="w-6" />
-          <p className="ml-2 mobile:text-sm tablet:text-xl">Learn by LIVE Do-it-Together Classes</p>
+          <p className="ml-2 mobile:text-sm tablet:text-xl">
+            Learn by LIVE Do-it-Together Classes
+          </p>
         </div>
         <div className="flex">
           <img src="./logos/rightTick.svg" className="w-6" />
-          <p className="ml-2 mobile:text-sm tablet:text-xl">Unlimited Practise Session to master skills</p>
+          <p className="ml-2 mobile:text-sm tablet:text-xl">
+            Unlimited Practise Session to master skills
+          </p>
         </div>
       </div>
     </div>
