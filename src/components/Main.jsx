e05fd6abafef30f24165a57e5087a09a1baec555
Kaushal Kumar Yadav <kaushalyad321@gmail.com>
2024-06-03 12:38:04 +0545

    completed

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -18,23 +18,23 @@ const Main = () => {
           </div>
         </div>
       </div>
-      <div className="flex flex-row mt-5 gap-6 justify-between items-center ">
-        <div className="flex flex-col justify-center items-center gap-4">
+      <div className="flex flex-row mt-5 gap-2 justify-evenly items-center ">
+        <div className="flex flex-col  w-1/3 justify-center items-center gap-4">
           <img src="./logos/video.svg" className=" mobile:w-12 tablet:w-20" />
-          <p className=" mobile:text-sm tablet:text-3xl">
+          <p className=" mobile:text-base tablet:text-3xl">
             Do-it-together, live on zoom
           </p>
         </div>
-        <div className="flex flex-col justify-center items-center gap-4">
+        <div className="flex flex-col w-1/3 justify-center items-center gap-4">
           <img src="./logos/star.svg" className=" mobile: w-12 tablet:w-20" />
           <div className=" flex flex-col">
-            <p className=" mobile:text-sm tablet:text-3xl">4.8 /5</p>
-            <p className=" mobile:text-sm tablet:text-3xl">Rated Classes</p>
+            <p className=" mobile:text-base tablet:text-3xl">4.8 /5</p>
+            <p className=" mobile:text-base tablet:text-3xl">Rated Classes</p>
           </div>
         </div>
-        <div className="flex flex-col justify-center items-center gap-4">
+        <div className="flex flex-col w-1/3 justify-center items-center gap-4">
           <img src="./logos/people.svg" className=" mobile:w-12 tablet:w-20" />
-          <p className=" mobile:text-sm tablet:text-3xl">35000+ Members</p>
+          <p className=" mobile:text-base tablet:text-3xl">35000+ Members</p>
         </div>
       </div>
       <div className="  mobile: w-[90%] tablet:w-[50%] mt-5">
